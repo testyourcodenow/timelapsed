6f02f9a9a794e6349520d1a7e303737c2abbe84f
Shaka Paul <mwafrikamzalendo@gmail.com>
2024-09-24 19:23:46 +0300

    feat: released version 2.0.0.

--- README.md
+++ README.md
@@ -3,72 +3,72 @@ Timelapsed is a NodeJs module that makes it easy to support fuzzy timestamps (e.
 
 ## usage
 ```javascript
-const TimeLapsed = require('timelapsed');
+import { fromDateString, fromTimestamp } from "timelapsed";
 
-TimeLapsed.fromTimestamp('1514811522125');
+fromTimestamp('1514811522125');
 
-TimeLapsed.fromDateString('2017-11-07 15:58:42.125836');
+fromDateString('2017-11-07 15:58:42.125836');
 ```
 
 #### methods available
 for *timestamps* eg "1514811522125" *string*
->use `TimeLapsed.fromTimestamp()`
+>use `fromTimestamp()`
 
 for *datestrings* eg "2019-01-02T21:00:00.000Z" *string*
->use `TimeLapsed.fromDateString()`
+>use `fromDateString()`
 
 #### required parameters
 **timestamp** *string* eg "1514811522125"
->when using `TimeLapsed.fromTimestamp(timestamp)`
+>when using `fromTimestamp(timestamp)`
 
 **datestring** *string* eg "2019-01-02T21:00:00.000Z"
->when using `TimeLapsed.fromDateString(datestring)`
+>when using `fromDateString(datestring)`
 
 #### optional parameters with default values
 **notation** `(default = 'lng')` *string*
 - *twitter*
     >"n", "m", "h", "d", "w"
     ```javascript
-    TimeLapsed.fromTimestamp(1514811522125, notation="twitter");
-    TimeLapsed.fromDateString('2019-01-02T21:00:00.000Z', notation='twitter');
+    fromTimestamp(1514811522125, 'twitter');
+    fromDateString('2019-01-02T21:00:00.000Z', 'twitter');
     ```
 - *mid*
     >"now", "min", "mins", "hr", "hrs", "dy", "dys", "wk", "wks"
     ```javascript
-    TimeLapsed.fromTimestamp(1514811522125, notation="mid");
-    TimeLapsed.fromDateString('2019-01-02T21:00:00.000Z', notation='mid');
+    fromTimestamp(1514811522125, 'mid');
+    fromDateString('2019-01-02T21:00:00.000Z', 'mid');
     ```
 - *lng*
     >"just now", "minute ago", "minutes ago", "hour ago", "hours ago", "yesterday", "days ago", "week ago", "weeks ago".
     ```javascript
-    TimeLapsed.fromTimestamp(1514811522125, notation="lng");
-    TimeLapsed.fromDateString('2019-01-02T21:00:00.000Z', notation='lng');
+    fromTimestamp(1514811522125, 'lng');
+    fromDateString('2019-01-02T21:00:00.000Z', 'lng');
     ```
 
-**unsafe** `(dafault = true)` *boolean*
+**safe** `(dafault = true)` *boolean*
 - will **Throw an Error** if parameter set to **true** and the **timestamp** or **datestring** provided is **invalid**.
     ```javascript
-    TimeLapsed.fromTimestamp('cf1529385554545xfgg', notation="twitter", unsafe=true);
+    fromTimestamp('cf1529385554545xfgg', 'twitter', false);
 
     Result:
     Error: Bad timestamp
     at Function.fromTimestamp...
 
-    TimeLapsed.fromDateString('xsdsd2017-11-07 15:58:42.125836', notation="lng", unsafe=true);
+    fromDateString('xsdsd2017-11-07 15:58:42.125836', "lng", false);
 
     Result:
     Error: Bad datestring
     at Function.fromDateString...
     ```
-- will return **'N/A'** *string* if parameter set to **false** and the **timestamp** or **datestring** provided is **invalid**.
+- will return **'N/A'** *string* if parameter set to **true** and the **timestamp** or **datestring** provided is **invalid**.
 
     ```javascript
-    TimeLapsed.fromTimestamp('1529385554545xfgg', notation="twitter", unsafe=false);
+    fromTimestamp('1529385554545xfgg', 'twitter', true);
 
     Result:
     'N/A'
 
-    TimeLapsed.fromDateString('xsdsd2017-11-07 15:58:42.125836', notation="lng", unsafe=false);
+    fromDateString('xsdsd2017-11-07 15:58:42.125836', 'lng', true);
 
     Result:
     'N/A'
--- src/timelapsed.js
+++ src/timelapsed.js
@@ -1,122 +1,211 @@
-'use strict';
-
-const TimeLapsed = {
-  version: "0.0.1", 
-  YEAR: String(new Date().getYear()).substr(1),
-  MONTHS: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-  MONTHSL: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-  LONG: {
-      NOW: "just now",
-      MIN: " min ago",
-      MINS: " mins ago",
-      HOUR: " hour ago",
-      HOURS: " hours ago",
-      DAY: "yesterday",
-      DAYS: " days ago"
-  },
-  MID: {
-        NOW: "just now",
-        MIN: "mn",
-        MINS: "mns",
-        HOUR: "hr",
-        HOURS: "hrs",
-        DAY: "dy",
-        DAYS: "dys"
-  },
-  SHORT: {
-        NOW: "now",
-        MIN: "m",
-        MINS: "m",
-        HOUR: "h",
-        HOURS: "h",
-        DAY: "d",
-        DAYS: "d"
-  },
-  valueOf: function(){
-    console.log("Time Lapsed Module.");
-  },
-  fromTimestamp: function(seconds, notation = undefined){
-    let secondsLapsed = (Date.now() - Number(seconds)) / 1000;
-    return this.deductSeconds(secondsLapsed, 'timestampStr', seconds, notation);
-  },
-  fromDateObject: function(dateObj, notation = undefined){
-    let secondsLapsed = (Date.now() - Date.parse(String(dateObj))) / 1000;
-    return this.deductSeconds(secondsLapsed, 'dateObjStr', dateObj, notation);
-  },
-  parseDateObj: function(rawObj, type){
-    let theDateObj = undefined;
-    if (type === 'dateObjStr') {
-      theDateObj = new Date(String(rawObj));
-    }else if (type === 'timestampStr') {
-      theDateObj = new Date(Number(rawObj));
-    }else{
-      throw new Error("Unknown datetime format!");
-    }
-    let theDate = theDateObj.getDate();
-    let theMonth = this.MONTHS[theDateObj.getMonth()];
-    let theYear = String(theDateObj.getYear()).substr(1);
-    if (theYear < this.YEAR)
-    	return theDate + ' ' + theMonth + ' ' + theYear;
-    else{
-    	if (theDate < 10)
-    		return '0' + theDate + ' ' + theMonth;
-    	else
-    		return theDate + ' ' + theMonth;
-    }
-  },
-  floorTimeCalc: function(timeCalced){
-    return Math.floor(timeCalced);
-  },
-  deductSeconds: function(secondsLapsed, type, rawObj, notation){
-    let timestrings = undefined;
-    if (notation === 'twitter') {
-      timestrings = this.SHORT;
-  	}else if (notation === 'mid'){
-  	  timestrings = this.MID;
-      }else if (notation === undefined || notation === null){
-        timestrings = this.LONG;
-      }else{
-        throw new Error("Unknown format!");
-      }
-      let postTime = undefined;
-      if (secondsLapsed <= 60) {
-        postTime = timestrings.NOW;
-      }else if (secondsLapsed > 60 && secondsLapsed < 3600) {
-        postTime = secondsLapsed / 60;
-        postTime = this.floorTimeCalc(postTime);
-        if (postTime === 1) {
-          postTime += timestrings.MIN;
-        }else{
-          postTime += timestrings.MINS;
-        }
-      }else if (secondsLapsed > 3600 && secondsLapsed < 86400) {
-        postTime = secondsLapsed / 3600;
-        postTime = this.floorTimeCalc(postTime);
-        if (postTime === 1) {
-          postTime += timestrings.HOUR;
-        }else{
-          postTime += timestrings.HOURS;
-        }
-      }else if (secondsLapsed > 86400 && secondsLapsed < 604800) {
-        postTime = secondsLapsed / 86400;
-        postTime = this.floorTimeCalc(postTime);
-        if (postTime === 1) {
-        	if (timestrings === this.LONG)
-        		postTime = timestrings.DAY;
-        	else
-          	postTime += timestrings.DAY;
-        }else{
-          postTime += timestrings.DAYS;
-        }
-      }else{
-        if (type === 'dateObjStr') {
-          postTime = this.parseDateObj(rawObj, type = 'dateObjStr');
-        }else if (type === 'timestampStr') {
-          postTime = this.parseDateObj(rawObj, type = 'timestampStr');
-        }
-      }
-      return postTime;
-    }
+const VERSION = "2.0.0";
+const MINUTE = 60;
+const HOUR = 3600;
+const DAY = 86400;
+const WEEK = 604800;
+const MONTH = 2629800;
+const YEAR = String(new Date().getYear()).substr(1);
+const MONTHS = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+const MONTHSL = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const LONG = {
+  NOW: "just now",
+  MIN: " minute ago",
+  MINS: " minutes ago",
+  HOUR: " hour ago",
+  HOURS: " hours ago",
+  DAY: "yesterday",
+  DAYS: " days ago",
+  WEEK: " week ago",
+  WEEKS: " weeks ago",
+};
+const MID = {
+  NOW: "now",
+  MIN: "min",
+  MINS: "mins",
+  HOUR: "hr",
+  HOURS: "hrs",
+  DAY: "dy",
+  DAYS: "dys",
+  WEEK: "wk",
+  WEEKS: "wks",
+};
+const SHORT = {
+  NOW: "n",
+  MIN: "m",
+  MINS: "m",
+  HOUR: "h",
+  HOURS: "h",
+  DAY: "d",
+  DAYS: "d",
+  WEEK: "w",
+  WEEKS: "w",
 };
 
-module.exports = TimeLapsed;
+const getVersion = () => {
+  return VERSION;
+};
+
+const parsedateStr = (rawObj, type, notation) => {
+  let thedateStr = undefined;
+  if (type === "dateStrStr") {
+    thedateStr = new Date(String(rawObj));
+  } else if (type === "timestampStr") {
+    thedateStr = new Date(Number(rawObj));
+  } else {
+    throw new Error("Unknown datetime format!");
+  }
+
+  let theDate = thedateStr.getDate();
+  let timestrings = undefined;
+  let monthStr = undefined;
+  if (notation === "twitter" || notation === "mid") {
+    monthStr = MONTHS;
+  } else if (
+    notation === "lng" ||
+    notation === undefined ||
+    notation === null
+  ) {
+    monthStr = MONTHSL;
+  } else {
+    throw new Error("Unknown notation format!");
+  }
+
+  let theMonth = monthStr[thedateStr.getMonth()];
+  let theYear = String(thedateStr.getYear()).substr(1);
+  if (theYear < YEAR) return theDate + " " + theMonth + " " + theYear;
+  else {
+    if (theDate < 10) return "0" + theDate + " " + theMonth;
+    else return theDate + " " + theMonth;
+  }
+};
+
+const floorTimeCalc = (timeCalced) => {
+  return Math.floor(timeCalced);
+};
+
+const deductSeconds = (secondsLapsed, type, rawObj, notation) => {
+  let timestrings = undefined;
+  if (notation === "twitter") {
+    timestrings = SHORT;
+  } else if (notation === "mid") {
+    timestrings = MID;
+  } else if (
+    notation === "lng" ||
+    notation === undefined ||
+    notation === null
+  ) {
+    timestrings = LONG;
+  } else {
+    throw new Error("Unknown notation format!");
+  }
+
+  let postTime = undefined;
+  if (secondsLapsed < MINUTE) {
+    postTime = timestrings.NOW;
+  } else if (secondsLapsed >= MINUTE && secondsLapsed < HOUR) {
+    postTime = secondsLapsed / MINUTE;
+    postTime = floorTimeCalc(postTime);
+    if (postTime === 1) {
+      postTime += timestrings.MIN;
+    } else {
+      postTime += timestrings.MINS;
+    }
+  } else if (secondsLapsed >= HOUR && secondsLapsed < DAY) {
+    postTime = secondsLapsed / HOUR;
+    postTime = floorTimeCalc(postTime);
+    if (postTime === 1) {
+      postTime += timestrings.HOUR;
+    } else {
+      postTime += timestrings.HOURS;
+    }
+  } else if (secondsLapsed >= DAY && secondsLapsed < WEEK) {
+    postTime = secondsLapsed / DAY;
+    postTime = floorTimeCalc(postTime);
+    if (postTime === 1) {
+      if (timestrings === LONG) postTime = timestrings.DAY;
+      else postTime += timestrings.DAY;
+    } else {
+      postTime += timestrings.DAYS;
+    }
+  } else if (secondsLapsed >= WEEK && secondsLapsed < MONTH) {
+    postTime = secondsLapsed / WEEK;
+    postTime = floorTimeCalc(postTime);
+    if (postTime === 1) {
+      if (timestrings === LONG) postTime += timestrings.WEEK;
+      else postTime += timestrings.WEEK;
+    } else {
+      postTime += timestrings.WEEKS;
+    }
+  } else {
+    if (type === "dateStrStr") {
+      postTime = parsedateStr(rawObj, (type = "dateStrStr"), notation);
+    } else if (type === "timestampStr") {
+      postTime = parsedateStr(rawObj, (type = "timestampStr"), notation);
+    }
+  }
+  return postTime;
+};
+
+export const fromTimestamp = (milliseconds, notation, safe) => {
+  if (typeof safe == undefined || typeof safe === null) {
+    safe = true;
+  }
+
+  if (isNaN(Number(milliseconds)) && safe == false) {
+    throw new Error("Bad timestamp");
+  } else if (isNaN(Number(milliseconds)) && safe == true) {
+    return "N/A";
+  }
+
+  if (typeof notation == undefined || typeof notation === null) {
+    notation = "lng";
+  }
+
+  let secondsLapsed = (Date.now() - Number(milliseconds)) / 1000;
+  return deductSeconds(secondsLapsed, "timestampStr", milliseconds, notation);
+};
+
+export const fromDateString = (dateStr, notation, safe) => {
+  if (typeof safe == undefined || typeof safe === null) {
+    safe = true;
+  }
+
+  if (isNaN(Date.parse(String(dateStr))) && safe == false) {
+    throw new Error("Bad datestring");
+  } else if (isNaN(Date.parse(String(dateStr))) && safe == true) {
+    return "N/A";
+  }
+
+  if (typeof notation == undefined || typeof notation === null) {
+    notation = "lng";
+  }
+
+  let secondsLapsed = (Date.now() - Date.parse(String(dateStr))) / 1000;
+  return deductSeconds(secondsLapsed, "dateStrStr", dateStr, notation);
+};
